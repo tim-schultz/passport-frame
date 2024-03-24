cd136bda6d50ec78666b709be0fbcd91fb5a27f6
jeedani <36130718+jeedani@users.noreply.github.com>
2024-03-24 23:52:35 +0900

    button update

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -16,9 +16,17 @@ const app = new Frog({
 // Uncomment to use Edge Runtime
 // export const runtime = 'edge'
 
+const tokens = ['ETH', 'WARP', 'DEGEN'];
+let currentIndex = 0;
+
 app.frame('/', (c) => {
   const { buttonValue, inputText, status } = c
   const token = inputText || buttonValue
+  
+  if (c.buttonValue === 'next') {
+  currentIndex = (currentIndex + 1) % tokens.length;
+  }
+
   return c.res({
     image: (
       <div
@@ -51,8 +59,9 @@ app.frame('/', (c) => {
           }}
         >
           {status === 'response'
-            ? `Nice choice.${token ? ` ${token.toUpperCase()}!!` : ''}`
-            : 'Welcome!'}
+            // ? `Let's go buy${tokens[currentIndex] ? ` ${token.toUpperCase()}!!` : ''}`
+            ? `Let's go buy ${tokens[currentIndex]}`
+            : 'Do you want to test your luck today?'}
         </div>
       </div>
     ),
@@ -61,8 +70,8 @@ app.frame('/', (c) => {
       // <Button value="apples">Apples</Button>,
       // <Button value="oranges">Oranges</Button>,
       // <Button value="bananas">Bananas</Button>,
-      <Button value="BTC">Feeling Lucky?</Button>,
-      status === 'response' && <Button.Reset>Reset</Button.Reset>,
+      <Button value="next">Feeling Lucky?</Button>,
+      status === 'response' && <Button.Redirect location="https://app.uniswap.org/?chain=optimism">BUY & HODL</Button.Redirect>,
     ],
   })
 })
